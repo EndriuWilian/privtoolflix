06cdf4ddb55fc93cab884ee6dcc5c5cfeabb2574
Endriu Wilian de Oliveira Machado <endriu.machado@facta.com.br>
2025-07-25 16:00:20 -0300

    fix removendo CORS de teste local

--- backend/src/main/java/com/privtoolflix/controller/ConsultaOmdbController.java
+++ backend/src/main/java/com/privtoolflix/controller/ConsultaOmdbController.java
@@ -28,8 +28,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-//@CrossOrigin(origins = "http://localhost:3000")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:3000")
 
 @RestController
 public class ConsultaOmdbController {
